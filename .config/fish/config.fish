3e39fbc80be7045256b1c8a8ae9e1ea1acc5e98f
onx <betterming.wang@gmail.com>
2022-04-01 00:27:11 +0800

    commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ fish_vi_key_bindings
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# eval /opt/miniconda2/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 
