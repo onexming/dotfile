0f63bbcd7254fd0182de9ca3baab4b376cff4835
onx <betterming.wang@gmail.com>
2022-04-07 00:28:06 +0800

    polybar

--- .screenlayout/home.sh
+++ .screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 2560x1440 --pos 0x0 --rotate left --output HDMI-0 --primary --mode 3440x1440 --pos 1440x618 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off
+xrandr --output HDMI-0 --mode 2560x1440 --pos 0x0 --rotate left --output DP-0 --primary --mode 3440x1440 --rate 144.00 --pos 1440x618 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
