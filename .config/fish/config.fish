ac3ae5a38f9a041359449c59b2a9508f514ffb0c
onx <betterming.wang@gmail.com>
2022-06-16 23:07:46 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,12 @@ end
 
 fish_vi_key_bindings
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-# eval /opt/miniconda2/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
-
 # fix clear command
 set -g -x TERMINFO /usr/share/terminfo
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+starship init fish | source
