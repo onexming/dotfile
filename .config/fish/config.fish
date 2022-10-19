c65db38dee0e1e519e660d1811b81ee83c0e6fa7
onx <betterming.wang@gmail.com>
2022-10-19 22:38:20 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,5 +13,6 @@ eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 bind -M insert \cf "eval (__fzfcmd) -q '(commandline)' | read -l result; and xdg-open \$result"
+string match -q "$TERM_PROGRAM" "vscode"
+and . (code --locate-shell-integration-path fish)
 starship init fish | source
-
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,9 +4,11 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -rq top &
+#polybar -rq top &
 
-polybar -rq left &
+#polybar -rq left &
+
+polybar -rq all &
 
 echo "Polybar launched..."
 
